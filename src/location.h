415fa5443883fc1098d71c56f7a55ba7d5cc2cff
Joachim Pileborg <arrow@pileborg.se>
2015-12-26 05:35:30 +0100

    Added the location class

---
+++ src/location.h
@@ -0,0 +1,17 @@
+//
+// Created by arrow on 2015-12-26.
+//
+
+#ifndef PTMUD_SCRATCH_LOCATION_H
+#define PTMUD_SCRATCH_LOCATION_H
+
+#include "basic_object.h"
+
+class location : public basic_object
+{
+public:
+    location() {}
+};
+
+
+#endif //PTMUD_SCRATCH_LOCATION_H
