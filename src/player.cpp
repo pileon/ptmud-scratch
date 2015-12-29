506be9540e9d60f72d89065fe558d0cc00634d72
Joachim Pileborg <arrow@pileborg.se>
2015-12-29 07:36:44 +0100

    Added player classe

---
+++ src/player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arrow on 2015-12-29.
+//
+
+#include "player.h"
---
+++ src/player.h
@@ -0,0 +1,20 @@
+//
+// Created by arrow on 2015-12-29.
+//
+
+#ifndef PTMUD_SCRATCH_PLAYER_H
+#define PTMUD_SCRATCH_PLAYER_H
+
+#include "character.h"
+
+class player : public character
+{
+public:
+
+protected:
+
+private:
+};
+
+
+#endif //PTMUD_SCRATCH_PLAYER_H
