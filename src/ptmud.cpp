4526411f4caa22df9c501cf8647ba4ee65e1be83
Joachim Pileborg <arrow@pileborg.se>
2016-01-03 00:23:56 +0100

    Added loop

--- src/ptmud.cpp
+++ src/ptmud.cpp
@@ -5,5 +5,13 @@
 int main()
 {
     net::init();
+
+    std::string s;
+    do
+    {
+        std::cout << "> " << std::flush;
+    } while (std::getline(std::cin, s) && s.length() > 0);
+    std::cout << "End!\n";
+
     net::clean();
 }
